6f11a31fd572f40163b597bbd1345a510deb6d79
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-02-01 15:34:54 +0300

    first commit

---
+++ src/components/ui/ButtonUi.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="button">{{ name }}</button>
+</template>
+
+<script>
+
+export default {
+  name: 'ButtonUi',
+  components: {
+  },
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  setup () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  background-color: orange;
+  padding: 5px 15px;
+  color: red;
+}
+</style>
